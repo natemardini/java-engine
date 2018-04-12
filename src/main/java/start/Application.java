704197242a5275ec374ca7fa15204718d9cdaa03
Chad Yehia <lex.yehia@icloud.com>
2018-04-12 13:29:40 -0700

    modded

--- src/main/java/start/Application.java
+++ src/main/java/start/Application.java
@@ -1,7 +1,27 @@
 package start;
 
-public class Application {
-    public static void main(String[] args) {
-        System.out.println("Hello!");
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+
+public class Application implements HttpHandler {
+    public static void main(String[] args) throws Exception {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
+        server.createContext("/", new Application());
+        server.setExecutor(null);
+        server.start();
+    }
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        String response = "This is the response";
+        exchange.sendResponseHeaders(200, response.length());
+        OutputStream os = exchange.getResponseBody();
+        os.write(response.getBytes());
+        os.close();
     }
 }
